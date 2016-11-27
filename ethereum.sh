e9f879ae59eed157b8ee7a788489cd83adfd53b4
Kenneth Falck <kennu@iki.fi>
2016-11-27 18:05:19 +0200

    Add sample startup script

---
+++ ethereum.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -xe
+# Launch Ethereum with the specified Network ID and a custom genesis block.
+# Kenneth Falck <kennu@luottamuksenloyly.fi> 2016
+cd /home/ubuntu
+geth init genesis.json
+geth --networkid "$1"
