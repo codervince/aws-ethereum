2f47067ad2fac27480a0168784da26230802536e
Kenneth Falck <kennu@iki.fi>
2016-11-27 20:53:22 +0200

    Update scripts

--- connect.sh
+++ connect.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# Check status of the Ethereum CloudFormation deployment
+# Conncet to Ethereum CloudFormation deployed node 1
 # Kenneth Falck <kennu@luottamuksenloyly.fi> 2016
 TEAM_ID="LL"
 NETWORK_ID="42424242"
 ADDR=`aws --profile luottamus cloudformation describe-stacks --stack-name Ethereum-$TEAM_ID-$NETWORK_ID --query "Stacks[0].Outputs[?OutputKey=='Node1PublicDnsName'].OutputValue" --output text`
-echo "Node1 up at $ADDR"
+ssh -i ethereum.pem ubuntu@$ADDR
--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@
 TEAM_ID="LL"
 NETWORK_ID="42424242"
 aws --profile luottamus cloudformation deploy --template-file ethereum-cf-stack.yml  --stack-name Ethereum-$TEAM_ID-$NETWORK_ID --parameter-overrides "TeamId=$TEAM_ID" "NetworkId=$NETWORK_ID" "KeyName=ethereum" "InstanceType=t2.medium" "ImageId=ami-07174474" "VolumeSize=20" "AvailabilityZone=eu-west-1a"
-aws --profile luottamus cloudformation describe-stacks --stack-name Ethereum-$TEAM_ID-$NETWORK_ID --query Stacks[0].Outputs[].OutputValue --output text
+./status.sh
