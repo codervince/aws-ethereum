1ddc3b29a3c18b63c09e53dbe74a86f1367e0d2c
Kenneth Falck <kennu@iki.fi>
2016-11-27 18:40:26 +0200

    Added logging

--- ethereum.sh
+++ ethereum.sh
@@ -2,5 +2,9 @@
 # Launch Ethereum with the specified Network ID and a custom genesis block.
 # Kenneth Falck <kennu@luottamuksenloyly.fi> 2016
 cd /home/ubuntu
-geth init genesis.json
-geth --networkid "$1"
+echo Initializing genesis block at `date` >> ethereum.log
+env >> ethereum.log
+geth init genesis.json >>ethereum.log 2>&1
+echo Launching geth at `date` >> ethereum.log
+geth --networkid "$1" >>ethereum.log 2>&1
+disown
