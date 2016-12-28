8e2649b4642b165ade6509df81c2fc7135311545
Kenneth Falck <kennu@iki.fi>
2016-12-28 21:25:00 +0200

    Enable mining

--- ethereum.sh
+++ ethereum.sh
@@ -11,7 +11,7 @@ echo Launching geth at `date` >> ethereum.log
 mkdir -p /home/ubuntu/.ethereum/keystore
 curl -o /home/ubuntu/.ethereum/keystore/UTC--2016-12-28T17-28-02.207258170Z-e986f163e65361be0f08aa48dcc3a7b12a57baf0 https://raw.githubusercontent.com/luottamus/aws-ethereum/master/account.json
 curl -o /home/ubuntu/account.passwd https://raw.githubusercontent.com/luottamus/aws-ethereum/master/account.passwd
-geth --port 30301 --networkid "$NETWORK_ID" --unlock e986f163e65361be0f08aa48dcc3a7b12a57baf0 --password /home/ubuntu/account.passwd >>ethereum.log 2>&1 &
+geth --port 30301 --networkid "$NETWORK_ID" --unlock e986f163e65361be0f08aa48dcc3a7b12a57baf0 --password /home/ubuntu/account.passwd --mine >>ethereum.log 2>&1 &
 disown
 sleep 5
 geth --exec 'console.log(JSON.stringify(admin.nodeInfo, null, 2))' attach | head -n -1 > /var/www/html/nodeinfo.json
