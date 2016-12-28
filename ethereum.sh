872ed82b9bfbc9ee63b5d96f0fd79e4223233f29
Kenneth Falck <kennu@iki.fi>
2016-12-28 19:39:45 +0200

    Add predefined account, move TCP port

--- ethereum.sh
+++ ethereum.sh
@@ -8,7 +8,9 @@ curl -s -o /var/www/html/ip.json 'https://api.ipify.org?format=json'
 echo Initializing genesis block at `date` >> ethereum.log
 geth init genesis.json >>ethereum.log 2>&1
 echo Launching geth at `date` >> ethereum.log
-geth --networkid "$NETWORK_ID" >>ethereum.log 2>&1 &
+geth --port 30301 --networkid "$NETWORK_ID" >>ethereum.log 2>&1 &
+mkdir -p /home/ubuntu/.ethereum/keystore
+curl -o /home/ubuntu/.ethereum/keystore/UTC--2016-12-28T17-28-02.207258170Z-48d8b7b8cd25ce99c6db70e1e373e6bfb51d1fbf https://raw.githubusercontent.com/luottamus/aws-ethereum/master/account.json
 disown
 sleep 5
 geth --exec 'console.log(JSON.stringify(admin.nodeInfo, null, 2))' attach | head -n -1 > /var/www/html/nodeinfo.json
