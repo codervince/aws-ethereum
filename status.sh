e2368b410b9c1025514ac9f62c80997b2f24dc14
Kenneth Falck <kennu@iki.fi>
2016-12-29 04:11:01 +0200

    Fix status script

--- status.sh
+++ status.sh
@@ -7,5 +7,9 @@ if [ "$TEAM_ID" = "" ]; then
   echo 'Example: deploy.sh team01'
   exit
 fi
-ADDR=`aws --profile luottamus cloudformation describe-stacks --stack-name Ethereum-$TEAM_ID --query "Stacks[0].Outputs[?OutputKey=='Node1PublicDnsName'].OutputValue" --output text`
+if [ "$TEAM_ID" = "master" ]; then
+  ADDR='master.ethereum.luottamuksenloyly.fi'
+else
+  ADDR=`aws --profile luottamus cloudformation describe-stacks --stack-name Ethereum-$TEAM_ID --query "Stacks[0].Outputs[?OutputKey=='Node1PublicDnsName'].OutputValue" --output text`
+fi
 echo "Node1 deployed at http://$ADDR"
