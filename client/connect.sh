b1adff5f8fd6cfe54d19ccc8588929ed94947508
Kenneth Falck <kennu@iki.fi>
2017-01-22 23:16:48 +0200

    Connect script update

--- client/connect.sh
+++ client/connect.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 # Connect to Ethereum CloudFormation deployed node 1
 # Kenneth Falck <kennu@luottamuksenloyly.fi> 2016-2017
-TEAM_ID="$1"
+[ -f /etc/ll-env ] && . /etc/ll-env
 if [ "$TEAM_ID" = "" ]; then
-  echo 'Usage: connect.sh <team-id>'
+  TEAM_ID="$1"
+fi
+if [ "$TEAM_ID" = "" ]; then
+  echo 'Usage: connect.sh [team-id]'
   echo 'Example: connect.sh team01'
   exit
 fi
 if [ "$TEAM_ID" = "master" ]; then
   ADDR='master.ethereum.luottamuksenloyly.fi'
 else
-  ADDR=`aws --profile luottamus cloudformation describe-stacks --stack-name Ethereum-$TEAM_ID --query "Stacks[0].Outputs[?OutputKey=='Node1PublicDnsName'].OutputValue" --output text`
+  ADDR=`aws cloudformation describe-stacks --stack-name Ethereum-$TEAM_ID --query "Stacks[0].Outputs[?OutputKey=='Node1PublicDnsName'].OutputValue" --output text`
 fi
-ssh -i ethereum.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ubuntu@$ADDR
+ssh -i ll-ethereum.pem -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ubuntu@$ADDR
