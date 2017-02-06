ed96a3f2fa814e9194cc1264dbab6f10b90a6f91
Kenneth Falck <kennu@iki.fi>
2017-02-06 02:49:10 +0200

    PORT in geth.sh

--- client/geth.sh
+++ client/geth.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+PORT=30301
 if [ -f "$HOME/Library/Application Support/Ethereum Wallet/binaries/Geth/unpacked/geth" ]; then
   # Mac
-  exec "$HOME/Library/Application Support/Ethereum Wallet/binaries/Geth/unpacked/geth" --datadir "$HOME/Library/Ethereum-LL" --port 30301 --rpcport 8645 --wsport 8646 --networkid 42424242 --fast --cache 512 --ipcpath $HOME/Library/Ethereum/geth.ipc --bootnodes enode://eeab46b802c5462f8aa75c7384bcb5d1b127b4e7365ae3b774595da23f5c3a22985acdc481432c91eca0618210a7d6ef6c2a711ebe4718f36b64bd15969540f6@52.208.196.206:30301 console
+  exec "$HOME/Library/Application Support/Ethereum Wallet/binaries/Geth/unpacked/geth" --datadir "$HOME/Library/Ethereum-LL" --port $PORT --rpcport 8645 --wsport 8646 --networkid 42424242 --fast --cache 512 --ipcpath $HOME/Library/Ethereum/geth.ipc --bootnodes enode://eeab46b802c5462f8aa75c7384bcb5d1b127b4e7365ae3b774595da23f5c3a22985acdc481432c91eca0618210a7d6ef6c2a711ebe4718f36b64bd15969540f6@52.208.196.206:$PORT console
 else
   # Linux
-  exec geth --datadir "$HOME/.ethereum-ll" --port 30301 --rpcport 8645 --wsport 8646 --networkid 42424242 --fast --cache 512 --ipcpath "$HOME/.ethereum/geth.ipc" --bootnodes enode://eeab46b802c5462f8aa75c7384bcb5d1b127b4e7365ae3b774595da23f5c3a22985acdc481432c91eca0618210a7d6ef6c2a711ebe4718f36b64bd15969540f6@52.208.196.206:30301 console
+  exec geth --datadir "$HOME/.ethereum-ll" --port $PORT --rpcport 8645 --wsport 8646 --networkid 42424242 --fast --cache 512 --ipcpath "$HOME/.ethereum/geth.ipc" --bootnodes enode://eeab46b802c5462f8aa75c7384bcb5d1b127b4e7365ae3b774595da23f5c3a22985acdc481432c91eca0618210a7d6ef6c2a711ebe4718f36b64bd15969540f6@52.208.196.206:$PORT console
 fi
