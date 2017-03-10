a7a8ebf253920a7d4a26fc597e6337b8fc19366b
Kenneth Falck <kennu@iki.fi>
2017-03-11 00:26:57 +0200

    Added guest key

--- cloud/ethereum.sh
+++ cloud/ethereum.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -xe
 # Launch Ethereum with the specified Network ID and a custom genesis block.
 # Kenneth Falck <kennu@luottamuksenloyly.fi> 2016-2017
-cd /home/ubuntu
+cd /home/ethereum
 echo "----------------------------------------------------------------------" >> ethereum.log
+if [ ! -f $HOME/.ssh/id_rsa ]; then
+  echo 'SSH key missing!'
+fi
 echo Detecting IP address >> ethereum.log
 curl -s -o /var/www/html/ip.json 'https://api.ipify.org?format=json'
 echo Downloading JDK >> ethereum.log
@@ -16,21 +19,21 @@ NATOPTION="--nat=extip:$PUBLICIP"
 echo Initializing genesis block at `date` >> ethereum.log
 geth init genesis.json >>ethereum.log 2>&1
 echo Launching geth at `date` >> ethereum.log
-mkdir -p /home/ubuntu/.ethereum/keystore
+mkdir -p /home/ethereum/.ethereum/keystore
 
-curl -s -o /home/ubuntu/account.passwd https://blockchain-bootcamp.com/files/cloud/account.passwd
+curl -s -o /home/ethereum/account.passwd https://blockchain-bootcamp.com/files/cloud/account.passwd
 if [ "$TEAM_ID" = "master" ]; then
   # Use predefined master account
-  curl -o /home/ubuntu/.ethereum/keystore/UTC--2016-12-28T17-28-02.207258170Z-$MASTER_ACCOUNT_ID https://blockchain-bootcamp.com/files/cloud/account.json
+  curl -o /home/ethereum/.ethereum/keystore/UTC--2016-12-28T17-28-02.207258170Z-$MASTER_ACCOUNT_ID https://blockchain-bootcamp.com/files/cloud/account.json
   BOOTNODES=""
   UNLOCK="--unlock $MASTER_ACCOUNT_ID"
 else
   # Create a random account for this node
-  geth --password /home/ubuntu/account.passwd account new
+  geth --password /home/ethereum/account.passwd account new
   BOOTNODES=`curl -s -d 'op=bootnode' http://master.blockchain-bootcamp.com/cgi-bin/ethereum.py`
   UNLOCK="--unlock 0"
 fi
-geth --port 30301 --networkid "$NETWORK_ID" $NATOPTION $BOOTNODES $UNLOCK --password /home/ubuntu/account.passwd --mine --minerthreads=1 >>ethereum.log 2>&1 &
+geth --port 30301 --networkid "$NETWORK_ID" $NATOPTION $BOOTNODES $UNLOCK --password /home/ethereum/account.passwd --mine --minerthreads=1 >>ethereum.log 2>&1 &
 disown
 sleep 5
 geth --exec 'console.log(JSON.stringify(admin.nodeInfo, null, 2))' attach | head -n -1 > /var/www/html/nodeinfo.json
